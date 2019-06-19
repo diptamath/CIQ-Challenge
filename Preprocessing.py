1b2cc69bbe303ac0e0631b2de816f50e178bd263
AYAN BASAK <ayanbasak13@gmail.com>
2019-06-19 19:02:24 +0530

    Add files via upload

---
+++ Preprocessing.py
@@ -0,0 +1,383 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from __future__ import absolute_import, division
+
+import os
+import time
+import numpy as np
+import pandas as pd
+import gensim
+from tqdm import tqdm
+from nltk.stem import PorterStemmer
+ps = PorterStemmer()
+from nltk.stem.lancaster import LancasterStemmer
+lc = LancasterStemmer()
+from nltk.stem import SnowballStemmer
+sb = SnowballStemmer("english")
+import gc
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Input, CuDNNLSTM, Embedding, Dropout, Activation, CuDNNGRU, Conv1D
+from keras.layers import Bidirectional, GlobalMaxPool1D, GlobalMaxPooling1D, GlobalAveragePooling1D
+from keras.layers import Input, Embedding, Dense, Conv2D, MaxPool2D, concatenate
+from keras.layers import Reshape, Flatten, Concatenate, Dropout, SpatialDropout1D
+from keras.optimizers import Adam
+from keras.models import Model
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras import initializers, regularizers, constraints, optimizers, layers
+
+import sys
+from os.path import dirname
+#sys.path.append(dirname(dirname(__file__)))
+from keras import initializers
+from keras.engine import InputSpec, Layer
+from keras import backend as K
+
+
+# In[2]:
+
+
+import spacy
+
+
+# In[ ]:
+
+
+spell_model = gensim.models.KeyedVectors.load_word2vec_format('wiki-news-300d-1M/wiki-news-300d-1M.vec')
+words = spell_model.index2word
+w_rank = {}
+
+for i,word in enumerate(words):
+    w_rank[word] = i
+    
+WORDS = w_rank
+
+
+# In[6]:
+
+
+# Use fast text as vocabulary
+def words(text): 
+    return re.findall(r'\w+', text.lower())
+
+def P(word): 
+    "Probability of `word`."
+    # use inverse of rank as proxy
+    # returns 0 if the word isn't in the dictionary
+    return - WORDS.get(word, 0)
+
+def correction(word): 
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word): 
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or [word])
+
+def known(words): 
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word): 
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+def singlify(word):
+    return "".join([letter for i,letter in enumerate(word) if i == 0 or letter != word[i-1]])
+
+
+# In[7]:
+
+
+def load_glove(word_dict, lemma_dict):
+    EMBEDDING_FILE = 'glove.840B.300d/glove.840B.300d.txt'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE))
+    embed_size = 300
+    nb_words = len(word_dict)+1
+    embedding_matrix = np.zeros((nb_words, embed_size), dtype=np.float32)
+    unknown_vector = np.zeros((embed_size,), dtype=np.float32) - 1.
+    print(unknown_vector[:5])
+    for key in tqdm(word_dict):
+        word = key
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.lower()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.upper()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.capitalize()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = ps.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = lc.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = sb.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = lemma_dict[key]
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        if len(key) > 1:
+            word = correction(key)
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[word_dict[key]] = embedding_vector
+                continue
+        embedding_matrix[word_dict[key]] = unknown_vector                    
+    return embedding_matrix, nb_words 
+
+
+# In[8]:
+
+
+def load_fasttext(word_dict, lemma_dict):
+    EMBEDDING_FILE = 'wiki-news-300d-1M/wiki-news-300d-1M.vec'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE) if len(o)>100)
+    embed_size = 300
+    nb_words = len(word_dict)+1
+    embedding_matrix = np.zeros((nb_words, embed_size), dtype=np.float32)
+    unknown_vector = np.zeros((embed_size,), dtype=np.float32) - 1.
+    print(unknown_vector[:5])
+    for key in tqdm(word_dict):
+        word = key
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.lower()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.upper()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.capitalize()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = ps.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = lc.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = sb.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = lemma_dict[key]
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        if len(key) > 1:
+            word = correction(key)
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[word_dict[key]] = embedding_vector
+                continue
+        embedding_matrix[word_dict[key]] = unknown_vector                    
+    return embedding_matrix, nb_words 
+
+
+# In[9]:
+
+
+def load_para(word_dict, lemma_dict):
+    EMBEDDING_FILE = 'paragram_300_sl999/paragram_300_sl999.txt'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE, encoding="utf8", errors='ignore') if len(o)>100)
+    embed_size = 300
+    nb_words = len(word_dict)+1
+    embedding_matrix = np.zeros((nb_words, embed_size), dtype=np.float32)
+    unknown_vector = np.zeros((embed_size,), dtype=np.float32) - 1.
+    print(unknown_vector[:5])
+    for key in tqdm(word_dict):
+        word = key
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.lower()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.upper()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = key.capitalize()
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = ps.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = lc.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = sb.stem(key)
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        word = lemma_dict[key]
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[word_dict[key]] = embedding_vector
+            continue
+        if len(key) > 1:
+            word = correction(key)
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[word_dict[key]] = embedding_vector
+                continue
+        embedding_matrix[word_dict[key]] = unknown_vector                    
+    return embedding_matrix, nb_words 
+
+
+# In[10]:
+
+
+train = pd.read_csv('ids_and_questions_dataset.csv').fillna(' ')
+
+
+# In[11]:
+
+
+train.head()
+
+
+# In[12]:
+
+
+start_time = time.time()
+print("Loading data ...")
+#train = pd.read_csv('ids_and_questions_dataset.csv').fillna(' ')
+#test = pd.read_csv('test.csv').fillna(' ')
+train_text = train['Questions']
+#test_text = test['question_text']
+text_list = train_text
+y = train['Label'].values
+num_train_data = y.shape[0]
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# In[13]:
+
+
+import spacy
+
+
+# In[ ]:
+
+
+#spacy.load('en_core_web_lg')
+
+
+# In[ ]:
+
+
+start_time = time.time()
+print("Spacy NLP ...")
+nlp = spacy.load('en_core_web_lg', disable=['parser','ner','tagger'])
+nlp.vocab.add_flag(lambda s: s.lower() in spacy.lang.en.stop_words.STOP_WORDS, spacy.attrs.IS_STOP)
+word_dict = {}
+word_index = 1
+lemma_dict = {}
+docs = nlp.pipe(text_list, n_threads = 2)
+
+
+# In[ ]:
+
+
+word_sequences = []
+
+for doc in tqdm(docs):
+    word_seq = []
+    for token in doc:
+        if (token.text not in word_dict) and (token.pos_ is not "PUNCT"):
+            word_dict[token.text] = word_index
+            word_index += 1
+            lemma_dict[token.text] = token.lemma_
+        if token.pos_ is not "PUNCT":
+            word_seq.append(word_dict[token.text])
+    word_sequences.append(word_seq)
+del docs
+gc.collect()
+train_word_sequences = word_sequences[:num_train_data]
+#test_word_sequences = word_sequences[num_train_data:]
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# In[ ]:
+
+
+# hyperparameters
+max_length = 55
+embedding_size = 600
+learning_rate = 0.001
+batch_size = 512
+num_epoch = 4
+
+train_word_sequences = pad_sequences(train_word_sequences, maxlen=max_length, padding='post')
+#test_word_sequences = pad_sequences(test_word_sequences, maxlen=max_length, padding='post')
+print(train_word_sequences[:1])
+#print(test_word_sequences[:1])
+#pred_prob = np.zeros((len(test_word_sequences),), dtype=np.float32)
+
